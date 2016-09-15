d83a3b76038e6cd06e4fa1c614ef7cca6f8ef3b9
Yariv Katz <yarivkatz@Yarivs-MacBook-Pro.local>
2016-09-15 13:31:23 +0300

    working on routing

--- lesson3/forms-and-chunks/src/app.component.ts
+++ lesson3/forms-and-chunks/src/app.component.ts
@@ -3,6 +3,7 @@
  */
 
 import {Component} from '@angular/core';
+import {TaskService, Task} from './services/task.service.ts';
 
 @Component({
     selector: 'app',
@@ -13,7 +14,28 @@ import {Component} from '@angular/core';
 })
 export class AppComponent{
     username : string = '';
+
+    title : string;
+    description : string;
+    group : string;
+    when : string;
+    tasks : Array<Task>;
+
+    constructor(private _taskService : TaskService){
+        this.tasks = _taskService.getTasks();
+    }
+
     submmittingForm(){
         alert(this.username);
+    }
+
+    createTask(){
+        const task = new Task(
+            this.title,
+            this.description,
+            this.group,
+            this.when
+        );
+        this._taskService.addTask(task);
     }
 }
--- lesson3/forms-and-chunks/src/app.module.ts
+++ lesson3/forms-and-chunks/src/app.module.ts
@@ -9,11 +9,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component.ts';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {FormsModule} from '@angular/forms';
+import {TaskService} from './services/task.service.ts';
 
 @NgModule({
     imports: [BrowserModule, FormsModule],
     declarations: [AppComponent],
     bootstrap: [AppComponent],
+    providers: [TaskService]
 })
 class AppModule{}
 
